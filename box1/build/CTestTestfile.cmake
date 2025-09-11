eb6f7f17080e566be0585f0c51bc6371281e1fc0
HP <a1343337038@163.com>
2025-09-12 00:11:24 +0800

    rviz建立文件，后续添加gazebo模 型文件

---
+++ box1/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/h/work/box1/src
+# Build directory: /home/h/work/box1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
